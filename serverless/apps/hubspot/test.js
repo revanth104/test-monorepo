bd7c48d98cb73a8e658f876d820704f37a56c8a0
rr31 <rr@cloudify.biz>
2024-01-08 18:02:41 +0530

    feat: testing

--- serverless/apps/hubspot/test.js
+++ serverless/apps/hubspot/test.js
@@ -1,13 +1,13 @@
 console.log("testing mono-repo");
 
 const title = "revanth kumareddy yanamala";
-
+const title2 = "revanth";
 const testing = () => {
   console.log(title);
 };
 
 const test = () => {
-  console.log("test");
+  console.log(title2);
 };
 
 testing();
